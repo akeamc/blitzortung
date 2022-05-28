f93bf18baa72c7b90de09cf2efa944489929cece
ThePicoNerd <ake@amcoff.net>
2022-05-28 23:15:06 +0200

    whoops

--- blitzortung/src/lib.rs
+++ blitzortung/src/lib.rs
@@ -1,5 +1,10 @@
-#![warn(clippy::pedantic)]
-#![warn(clippy::nursery)]
+#![warn(
+    clippy::pedantic,
+    clippy::nursery,
+    missing_docs,
+    missing_debug_implementations,
+    unreachable_pub
+)]
 
 #[cfg(feature = "live")]
 pub mod live;
